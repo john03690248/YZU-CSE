12c0318a72834af3aa0a4ee931ae875e45b9f78b
Johnson <john03690248@gmail.com>
2022-05-18 18:16:51 +0800

    8

--- 1102 Assembly Language and Computer Organization/1091502_proj_2/1091502_proj_2.cpp
+++ 1102 Assembly Language and Computer Organization/1091502_proj_2/1091502_proj_2.cpp
@@ -3,14 +3,15 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <math.h>
 using namespace std;
 struct instr {
 	string label;
 	string action;
 	string positions[3];
+	int numPastLabel = 0;
 };
 struct state {
-	string label;
 	string history = "000";
 	string states[8] = { "SN","SN","SN","SN","SN","SN","SN","SN" };
 	int numMisprediction = 0;
@@ -21,7 +22,6 @@ struct variable {
 	int value;
 };
 int existVariable(vector <variable> variableDatabase, string name);
-int existState(vector <state> stateDatabase, string label);
 int toDecimal(string binary);
 int main() {
 	ifstream in;
@@ -30,14 +30,15 @@ int main() {
 	vector <instr> instrDatabase;
 	vector <state> stateDatabase;
 	vector <variable> variableDatabase;
-	state tempState;
 
+	int numPastLabel_ = 0;
 	while (getline(in, line)) {    //transform the data into the specific format
 		instr tempInstr;
 		istringstream stream(line);
 		stream >> str;
 		tempInstr.label = str;
 		if (line[0] == '\t') {
+			numPastLabel_++;
 			tempInstr.label = str.substr(0, str.length() - 1);
 			instrDatabase.push_back(tempInstr);
 			continue;
@@ -54,20 +55,33 @@ int main() {
 				count++;
 			}
 		tempInstr.positions[count] = str.substr(temp, str.length() - temp);
+		tempInstr.numPastLabel = numPastLabel_;
 		instrDatabase.push_back(tempInstr);
 	}
 
 	int entry;
 	cout << "Please input entry(entry > 0):" << endl;
 	cin >> entry;
+	state tempState;
+
+	for (int i = 0; i < entry; i++) {
+		tempState.entry = i;
+		stateDatabase.push_back(tempState);
+	}
 
 	variable tempVarible;    //add R0(x0) to the variableDatabase
 	tempVarible.name = "R0";
 	tempVarible.value = 0;
 	variableDatabase.push_back(tempVarible);
 
+	string predictorResult;
+	string result;
 	for (int i = 0; i < instrDatabase.size(); i++) {    // 6 solutions include "li" , "add" , "addi" , "andi" , "beq" , "bne"
+		int tempi = i;
+		if (instrDatabase[i].action == "")
+			continue;
 		if (instrDatabase[i].action == "li") {
+			result = "N";
 			int record = existVariable(variableDatabase, instrDatabase[i].positions[0]);
 			if (record == -1) {
 				variable tempVarible;
@@ -79,6 +93,7 @@ int main() {
 				variableDatabase[record].value = stoi(instrDatabase[i].positions[1]);
 		}
 		else if (instrDatabase[i].action == "add") {
+			result = "N";
 			int record = existVariable(variableDatabase, instrDatabase[i].positions[0]);
 			if (record == -1) {
 				variable tempVarible;
@@ -88,6 +103,7 @@ int main() {
 			variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[0])].value = variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[1])].value + variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[2])].value;
 		}
 		else if (instrDatabase[i].action == "addi") {
+			result = "N";
 			int record = existVariable(variableDatabase, instrDatabase[i].positions[0]);
 			if (record == -1) {
 				variable tempVarible;
@@ -97,6 +113,7 @@ int main() {
 			variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[0])].value = variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[1])].value + stoi(instrDatabase[i].positions[2]);
 		}
 		else if (instrDatabase[i].action == "andi") {
+			result = "N";
 			int record = existVariable(variableDatabase, instrDatabase[i].positions[0]);
 			if (record == -1) {
 				variable tempVarible;
@@ -106,25 +123,8 @@ int main() {
 			variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[0])].value = variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[1])].value % (stoi(instrDatabase[i].positions[2]) + 1);
 		}
 		else if (instrDatabase[i].action == "beq" || instrDatabase[i].action == "bne") {
-			int tempi = i;
-			int record = existState(stateDatabase, instrDatabase[i].label);
-			string predictorResult;
-			string result;
-			if (record == -1) {
-				state tempState;
-				tempState.label = instrDatabase[i].label;
-				stateDatabase.push_back(tempState);
-				record = existState(stateDatabase, instrDatabase[i].label);
-			}
-
-			string tempState_ = stateDatabase[record].states[toDecimal(stateDatabase[record].history)];    //predictor result
-			if (tempState_ == "SN" || tempState_ == "WN")
-				predictorResult = "N";
-			else if (tempState_ == "ST" || tempState_ == "WT")
-				predictorResult = "T";
-
 			if (instrDatabase[i].action == "beq") {    //true result
-				if (variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[0])].value == variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[1])].value) {  
+				if (variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[0])].value == variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[1])].value) {
 					result = "T";
 					for (int j = 0; j < instrDatabase.size(); j++)
 						if (instrDatabase[j].label == instrDatabase[i].positions[2]) {
@@ -136,7 +136,7 @@ int main() {
 					result = "N";
 			}
 			else {
-				if (variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[0])].value != variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[1])].value) {   
+				if (variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[0])].value != variableDatabase[existVariable(variableDatabase, instrDatabase[i].positions[1])].value) {
 					result = "T";
 					for (int j = 0; j < instrDatabase.size(); j++)
 						if (instrDatabase[j].label == instrDatabase[i].positions[2]) {
@@ -147,46 +147,48 @@ int main() {
 				else
 					result = "N";
 			}
-
-			if (predictorResult != result)    //misprediction
-				stateDatabase[record].numMisprediction++;
-
-			cout << "entry: " << stateDatabase[record].entry << "       " << instrDatabase[tempi].action << " " << instrDatabase[tempi].positions[0] << "," << instrDatabase[tempi].positions[1] << "," << instrDatabase[tempi].positions[2] << endl;    //output
-			cout << "(" << stateDatabase[record].history << ", " << stateDatabase[record].states[0] << ", " << stateDatabase[record].states[1] << ", " << stateDatabase[record].states[2] << ", " << stateDatabase[record].states[3] << ", " << stateDatabase[record].states[4] << ", "
-				<< stateDatabase[record].states[5] << ", " << stateDatabase[record].states[6] << ", " << stateDatabase[record].states[7] << ") " << predictorResult << " " << result << "  misprediction: " << stateDatabase[record].numMisprediction << endl << endl;
-
-			if (result == "T") {    //state
-				string _tempState = stateDatabase[record].states[toDecimal(stateDatabase[record].history)];
-				if (_tempState == "WT") stateDatabase[record].states[toDecimal(stateDatabase[record].history)] = "ST";
-				else if (_tempState == "WN") stateDatabase[record].states[toDecimal(stateDatabase[record].history)] = "WT";
-				else if (_tempState == "SN") stateDatabase[record].states[toDecimal(stateDatabase[record].history)] = "WN";
-			}
-			else {
-				string _tempState = stateDatabase[record].states[toDecimal(stateDatabase[record].history)];
-				if (_tempState == "WN") stateDatabase[record].states[toDecimal(stateDatabase[record].history)] = "SN";
-				else if (_tempState == "WT") stateDatabase[record].states[toDecimal(stateDatabase[record].history)] = "WN";
-				else if (_tempState == "ST") stateDatabase[record].states[toDecimal(stateDatabase[record].history)] = "WT";
-			}
-
-			stateDatabase[record].history[0] = stateDatabase[record].history[1];    //history
-			stateDatabase[record].history[1] = stateDatabase[record].history[2];
-			if (result == "T")
-				stateDatabase[record].history[2] = '1';
-			else
-				stateDatabase[record].history[2] = '0';
-
 		}
+		int currentEntry = (tempi - instrDatabase[tempi].numPastLabel) % entry;
+		string tempState_ = stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)];    //predictor result
+		if (tempState_ == "SN" || tempState_ == "WN")
+			predictorResult = "N";
+		else if (tempState_ == "ST" || tempState_ == "WT")
+			predictorResult = "T";
+
+		if (predictorResult != result)    //misprediction
+			stateDatabase[currentEntry].numMisprediction++;
+
+		cout << "entry: " << stateDatabase[currentEntry].entry << "       " << instrDatabase[tempi].action << " " << instrDatabase[tempi].positions[0] << "," << instrDatabase[tempi].positions[1];    //output  
+		if (instrDatabase[tempi].positions[2] != "") 
+			cout << "," << instrDatabase[tempi].positions[2];
+		cout << endl;
+		cout << "(" << stateDatabase[currentEntry].history << ", " << stateDatabase[currentEntry].states[0] << ", " << stateDatabase[currentEntry].states[1] << ", " << stateDatabase[currentEntry].states[2] << ", " << stateDatabase[currentEntry].states[3] << ", " << stateDatabase[currentEntry].states[4] << ", "
+			<< stateDatabase[currentEntry].states[5] << ", " << stateDatabase[currentEntry].states[6] << ", " << stateDatabase[currentEntry].states[7] << ") " << predictorResult << " " << result << "  misprediction: " << stateDatabase[currentEntry].numMisprediction << endl << endl;
+
+		if (result == "T") {    //state
+			string _tempState = stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)];
+			if (_tempState == "WT") stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)] = "ST";
+			else if (_tempState == "WN") stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)] = "WT";
+	     	else if (_tempState == "SN") stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)] = "WN";
+		}
+		else {
+			string _tempState = stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)];
+			if (_tempState == "WN") stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)] = "SN";
+			else if (_tempState == "WT") stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)] = "WN";
+			else if (_tempState == "ST") stateDatabase[currentEntry].states[toDecimal(stateDatabase[currentEntry].history)] = "WT";
+		}
+
+		stateDatabase[currentEntry].history[0] = stateDatabase[currentEntry].history[1];    //history
+		stateDatabase[currentEntry].history[1] = stateDatabase[currentEntry].history[2];
+		if (result == "T")
+			stateDatabase[currentEntry].history[2] = '1';
+		else
+			stateDatabase[currentEntry].history[2] = '0';
 	}
 }
 int existVariable(vector <variable> variableDatabase, string name) {
 	for (int i = 0; i < variableDatabase.size(); i++) 
 		if (name == variableDatabase[i].name)
-			return i;
-	return -1;
-}
-int existState(vector <state> stateDatabase, string label) {
-	for (int i = 0; i < stateDatabase.size(); i++)
-		if (label == stateDatabase[i].label)
 			return i;
 	return -1;
 }
